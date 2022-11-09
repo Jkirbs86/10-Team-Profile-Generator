fe6e8b995888563d39d4fed21cdd944d580288c9
Jared Kirby <51921860+Jkirbs86@users.noreply.github.com>
2022-11-08 20:50:35 -0700

    test done

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -2,18 +2,18 @@ const Manager = require('../library/manager');
 
 test("Can create an office number.", () => {
     const testOfficeNumber = 2;
-    const employeeInstance = new Manager("Taylor", 2, "taylorpterrill@gmail.com", testOfficeNumber);
+    const employeeInstance = new Manager("Jared", 2, "Jared.Kirby99@gmail.com", testOfficeNumber);
     expect(employeeInstance.officeNumber).toBe(testOfficeNumber);
 });
 
 test("Testing officeNumber will return office number.", () => {
     const testOfficeNumber = 2;
-    const employeeInstance = new Manager("Taylor", 2, "taylorpterrill@gmail.com", testOfficeNumber);
+    const employeeInstance = new Manager("Jared", 2, "Jared.Kirby99@gmail.com", testOfficeNumber);
     expect(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
 });
 
 test("Testing role.", () => {
     const returnValue = "Manager";
-    const employeeInstance = new Manager("Taylor", 2, "taylorpterrill@gmail.com", 2);
+    const employeeInstance = new Manager("Jared", 2, "Jared.Kirby99@gmail.com", 2);
     expect(employeeInstance.getRole()).toBe(returnValue);
 });
